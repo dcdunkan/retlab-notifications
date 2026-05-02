2bd8c676b4f62a33b97f93e616f2664e21571028
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2026-05-03 03:01:47 +0530

    chore: update ret keys

--- scripts/fetch-keys.ts
+++ scripts/fetch-keys.ts
@@ -1,6 +1,11 @@
 const RET_API_URL = Deno.args[0];
 if (RET_API_URL == null) {
-	throw new Error("Pass Ret API URL as the only argument. E.g.: https://ret.dunked.dev/api");
+	console.error(
+		"%cerror%c: Pass Ret API URL as the only argument. E.g.: https://ret.dunked.dev/api",
+		"color: red",
+		"color: none",
+	);
+	Deno.exit(1);
 }
 
 const response = await fetch(RET_API_URL + "/keys");
